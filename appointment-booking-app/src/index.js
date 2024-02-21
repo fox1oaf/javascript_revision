ffada59c67658e74e09445dfd901d4b1aa025bea
fox1oaf <makifoxloaf@gmail.com>
2024-02-21 18:22:45 +0000

    appointment booking app

--- appointment-booking-app/src/index.js
+++ appointment-booking-app/src/index.js
@@ -1,10 +1,21 @@
 // express is creating the server so we don't need to do the job
 const express = require('express');
 const app = express()
+const homeController = require('../src/controllers/homeControllers')
+const appointmentsController = require('../src/controllers/appointmentsControllers')
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send ('hello world')
+
+    const data = homeController.getUsers();
+
+    return res.json (data)
+});
+
+app.get('/appointments', (req, res) => {
+
+    const data = appointmentsController.getAppointments();
+    return res.json (data)
 });
 
 app.listen(port, () => {
